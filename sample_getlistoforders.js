4c1b3b61bceb6b41cef49598979cfb0e9063b516
daishi <daishi@axlight.com>
2014-10-30 00:38:45 +0900

    to use sf.set()

--- sample_getlistoforders.js
+++ sample_getlistoforders.js
@@ -5,8 +5,8 @@ var mwsOrderAPI = require('./lib/orders.js');
 var marketplaceId = "ATVPDKIKX0DER";
 
 var sf = new mwsOrderAPI.requests.ListOrders({"marketplaceId": marketplaceId});
-sf.params.MarketplaceId.value = marketplaceId;
-sf.params.CreatedAfter.value = "2014-07-13";
+sf.set("MarketplaceId", marketplaceId);
+sf.set("CreatedAfter", "2014-07-13");
 client.invoke(sf, function(RESULT){
   console.log("--------");
   console.log(JSON.stringify(RESULT));
