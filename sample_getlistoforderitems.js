b017f1172ee9236a1c591654c6175fc68b5e941e
daishi <daishi@axlight.com>
2014-10-30 00:38:45 +0900

    to use sf.set()

--- sample_getlistoforderitems.js
+++ sample_getlistoforderitems.js
@@ -5,7 +5,7 @@ var mwsOrderAPI = require('./lib/orders.js');
 var marketplaceId = "ATVPDKIKX0DER";
 
 var sf = new mwsOrderAPI.requests.ListOrderItems();
-sf.params.AmazonOrderId.value = "111-1715221-5800265";
+sf.set("AmazonOrderId", "111-1715221-5800265");
 client.invoke(sf, function(RESULT){
   console.log("--------");
   console.log(JSON.stringify(RESULT));
